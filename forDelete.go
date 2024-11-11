8603499844f527e698c6d4edcb7cbf00adf7c630
IPampurin <i.pampurin@yandex.ru>
2024-11-11 11:07:46 +0500

    Add a file from work.

---
+++ forDelete.go
@@ -0,0 +1,16 @@
+// этот файл надо безжалостно удалить
+
+package main
+
+import (
+	"fmt"
+)
+
+// непонятно, зачем тут эта функция
+func Add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Я - домашка!")
+}
